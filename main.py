60d80eff4dcdf73be41092d495dc4d7ef9e3725b
StippMan <ra102919@uem.br>
2020-01-25 20:55:44 -0300

    funcoes de adicionar objetos no canvas funciona, printar na tela tbm, os botoes ainda nao

--- main.py
+++ main.py
@@ -1,43 +1,117 @@
 from tkinter import *
+from math import sqrt
+
 # from PIL import ImageTk, Image
 
-window = Tk() 
-window.title("Visualizer")
+class DrawnShape():
+	def __init__(self):
+		self.coordinates = [(0,0)]
 
-clearButton = Button(window,text="CLEAR")
-clearButton.grid(column = 1, row = 0)
+	def draw(self, canvas):
+		for pos in range(len(self.coordinates)):
+			point1 = self.coordinates[pos]
+			point2 = self.coordinates[pos-1]
+			canvas.create_line(point1[0],point1[1], point2[0],point2[1], fill="black", width=2)
 
 
-# rotateIcon = ImageTk.PhotoImage(Image.open("rotateIcon.png"))
-rotateButton = Button(window,text="ROTATE")
-rotateButton.grid(column = 2, row = 0)
+class Line(DrawnShape):
+	def __init__(self, x1,y1, x2,y2):
+		self.coordinates = [(x1,y1), (x2,y2)]
+
+class Triangle(DrawnShape):
+	def __init__(self, x1,y1, x2,y2, x3,y3):
+		self.coordinates = [(x1,y1), (x2,y2), (x3,y3)]
+
+class Rectangle(DrawnShape):
+	def __init__(self, x1,y1, x2,y2):
+		self.coordinates = [(x1,y1), (x2,y1), (x2,y2), (x1,y2)]
+
+class Circle(DrawnShape):
+	def __init__(self, x1,y1, radius):
+		self.coordinates = (x1,y1)
+		self.radius = radius
+		# self.radius = sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+	def draw(self, canvas):
+		x1 = self.coordinates[0] - self.radius
+		y1 = self.coordinates[1] - self.radius
+		x2 = self.coordinates[0] + self.radius
+		y2 = self.coordinates[1] + self.radius
+		canvas.create_oval(x1, y1, x2, y2, width=2,outline="black")
 
 
-scaleButton = Button(window,text="SCALE")
-scaleButton.grid(column = 3, row = 0)
+def addShape(shapes, shape):
+	shapes.append(shape)
 
-moveButton = Button(window,text="MOVE")
-moveButton.grid(column = 4, row = 0)
+def drawShapes(canvas, shapes):
+	for shape in shapes:
+		shape.draw(canvas)
 
-zoomButton = Button(window,text="ZOOM")
-zoomButton.grid(column = 4, row = 0)
+def createCanvas(root):
+	canvas = Canvas(root, bg="white")
+	canvas.grid(row=1,column=1)
 
-lbl1 = Label(window, text="DESENHOS")
-lbl1.grid(column=0, row=1)
+	return canvas
 
-lineButton = Button(window,text="LINE")
-lineButton.grid(column = 0, row = 2)
+def createToolbar(root):
+	toolbar = Frame(root, relief=SUNKEN)
 
-triangleButton = Button(window,text="TRIANGLE")
-triangleButton.grid(column = 0, row = 3)
+	clearButton = Button(toolbar,text="CLEAR")
+	rotateButton = Button(toolbar,text="ROTATE")
+	scaleButton = Button(toolbar,text="SCALE")	
+	moveButton = Button(toolbar,text="MOVE")
+	zoomButton = Button(toolbar,text="ZOOM")
 
-rectangleButton = Button(window,text="RECTANGLE")
-rectangleButton.grid(column = 0, row = 4)
+	clearButton.pack(side=LEFT, padx=2, pady=2)
+	rotateButton.pack(side=LEFT, padx=2, pady=2)
+	scaleButton.pack(side=LEFT, padx=2, pady=2)
+	moveButton.pack(side=LEFT, padx=2, pady=2)
+	zoomButton.pack(side=LEFT, padx=2, pady=2)
 
-circleButton = Button(window,text="CIRCLE")
-circleButton.grid(column = 0, row = 5)
+	toolbar.grid(row=0,column=0, columnspan=2, sticky=NSEW)
 
-canvas = Canvas(window, width = 500, height = 500, background = "black")
-canvas.grid_configure(column=1, row=1, columnspan=10, rowspan=10)
+	return toolbar
 
-window.mainloop()
+def createStatusbar(root):
+	statusbar = Label(root, text="", bd=1, relief=SUNKEN, anchor=W)
+	statusbar.grid(row=2,column=0, columnspan=2, sticky=NSEW)
+
+	return statusbar
+
+def createShapebar(root):
+
+	shapebar = Frame(root, bg="grey")
+
+	lbl1 = Label(shapebar, text="SHAPES", bg= "grey")
+	lbl1.pack(side=TOP, padx=2, pady=2)
+
+	lineButton = Button(shapebar,text="LINE")
+	triangleButton = Button(shapebar,text="TRIANGLE")
+	rectangleButton = Button(shapebar,text="RECTANGLE")
+	circleButton = Button(shapebar,text="CIRCLE")
+
+	lineButton.pack(side=TOP, padx=2, pady=2, fill=X)
+	triangleButton.pack(side=TOP, padx=2, pady=2, fill=X)
+	rectangleButton.pack(side=TOP, padx=2, pady=2, fill=X)
+	circleButton.pack(side=TOP, padx=2, pady=2, fill=X)
+
+	shapebar.grid(row=1,column=0, sticky=NSEW)
+
+	return shapebar
+
+def runApp():
+	shapes = []
+
+	root = Tk() 
+	root.title("Visualizer")
+
+	toolbar = createToolbar(root)
+	statusbar = createStatusbar(root)
+	shapebar = createShapebar(root)
+	canvas = createCanvas(root)
+	shapes.append(Circle(100,100,50))
+	drawShapes(canvas,shapes)
+	
+	root.mainloop()
+
+runApp()
